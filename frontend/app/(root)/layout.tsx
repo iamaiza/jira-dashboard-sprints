7ef9dc3a9e28768b92463123b1881720e8d6cac5
Aiza Ali <aiza9382@gmail.com>
2024-03-25 08:39:22 +0500

    user profile

---
+++ frontend/app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Navbar from "@/components/Navbar";
+import { CurrentUserContextProvider } from "@/context/CurrentUserContext";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <CurrentUserContextProvider>
+      <div className="px-5 max-sm:px-3">
+        <Navbar />
+        <main className="mt-24">{children}</main>
+      </div>
+    </CurrentUserContextProvider>
+  );
+}
