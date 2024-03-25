a1483199e89188a7cd4ae5d077812d14c58821d6
Aiza Ali <aiza9382@gmail.com>
2024-03-25 08:41:24 +0500

    sprints

---
+++ frontend/app/(root)/sprints/[id]/page.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { SprintProps, User } from "@/types/types";
+import { SPRINT, USERS } from "@/utils/query-mutations";
+import { useQuery } from "@apollo/client";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const SprintDetailPage = () => {
+    const pathname = usePathname()
+    // const router = useRouter()
+    const { data } = useQuery(SPRINT, {
+        variables: {
+            id: pathname.split("/")[2]
+        }
+    })
+    const { data: users } = useQuery(USERS)
+    const [sprint, setSprints] = useState<SprintProps | null>(null)
+    const [sprintUsers, setSprintUsers] = useState([])
+
+    useEffect(() => {
+        if(data?.sprint) setSprints(data.sprint);
+        if(users?.users) setSprintUsers(users.users);
+    }, [data?.sprint, users])
+
+  return (
+    <div className="mt-32">
+      <h1 className="text-[1.6rem] font-semibold">{sprint?.title}</h1>
+      <p className="text-gray-500 text-sm" title={sprint?.description}>
+        {sprint?.description?.substr(0, 140)}{sprint?.description && "..."}
+      </p>
+      <div className="mt-5 flexStart gap-3">
+        <input
+          className="w-40 mb-0 bg-gray-200 focus:w-52"
+          type="text"
+          placeholder="Search"
+        />
+        <div className="flexStart">
+          {sprintUsers.map((user: User) => (
+            <>
+              {user.imgUrl ? (
+                <Image
+                  className="rounded-full -ml-1"
+                  src={user.imgUrl}
+                  alt={user.name}
+                  width={36}
+                  height={36}
+                />
+              ) : (
+                <div className="uppercase tracking-widest w-9 h-9 bg-pink-600 rounded-full flexCenter text-[13px] font-bold -ml-2 first:ml-0">
+                  {user?.name.substr(0, 2)}
+                </div>
+              )}
+            </>
+          ))}
+        </div>
+      </div>
+      <div className={`flexBetween gap-5 mt-10`}>
+        {sprint?.status.map((st) => (
+          <div
+            key={sprint.id}
+            className=" bg-gray-200 min-h-[30rem] py-3 px-4 flex-1"
+          >
+            <div className="font-bold text-center mb-5">{st}</div>
+            {/* {filterTasksByStatus(st).map((task) => (
+              <div key={task?.id} className="p-3 bg-slate-800 font-normal">
+                <div>{task?.title}</div>
+                <div className="mt-5 capitalize">{task?.priority}</div>
+                <div className="mt-5">
+                  {task?.assigneeId?.imgUrl ? (
+                    <Image
+                      className="rounded-full ml-auto"
+                      src={task?.assigneeId?.imgUrl}
+                      alt={task?.assigneeId?.name}
+                      width={36}
+                      height={36}
+                    />
+                  ) : (
+                    <div className="uppercase tracking-widest w-9 h-9 bg-sky-900 rounded-full flexCenter text-[13px] font-bold ml-auto">
+                      {task?.assigneeId?.name?.substr(0, 2)}
+                    </div>
+                  )}
+                </div>
+              </div>
+            ))} */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SprintDetailPage;
---
+++ frontend/components/Create-Sprint.tsx
@@ -0,0 +1,166 @@
+"use client";
+import { SprintProps } from "@/types/types";
+import { CREATE_SPRINT, SPRINTS } from "@/utils/query-mutations";
+import { useMutation } from "@apollo/client";
+import { ChangeEvent, MouseEvent, useState } from "react";
+
+const CreateSprintModal = ({
+  setShowModal,
+}: {
+  setShowModal: (show: boolean) => void;
+}) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [statusCheck, setStatusCheck] = useState<string[]>([]);
+  const [error, setError] = useState("");
+  const [createSprintMutation] = useMutation(CREATE_SPRINT, {
+    refetchQueries: [{ query: SPRINTS }],
+    onCompleted: () => {
+      setShowModal(false)
+    }
+  });
+
+  const statusCheckHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = e.target;
+    if (checked) {
+      setStatusCheck((prevStatus) => [...prevStatus, value]);
+    } else
+      setStatusCheck((prevStatus) =>
+        prevStatus.filter((status) => status !== value)
+      );
+  };
+
+  const createSprintHandler = async (e: MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (!title.trim()) {
+        setError("Title is required");
+        return;
+      }
+      if (statusCheck.length <= 0) {
+        setError("Please select at least one status");
+        return;
+      }
+      const endDate = new Date(
+        Date.now() + 7 * 24 * 60 * 60 * 1000
+      ).toISOString();
+       
+      const { data } = await createSprintMutation({
+        variables: {
+          data: {
+            title,
+            description,
+            status: statusCheck,
+            startDate: new Date(Date.now()).toISOString(),
+            endDate,
+          },
+        },
+      });
+      // setShowModal(false);
+    } catch (error: any) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="bg-white absolute left-1/2 -translate-x-1/2 z-30 max-w-lg w-full p-5 rounded">
+      <form className="w-full" onSubmit={createSprintHandler}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Sprint Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          className="mb-0.5"
+          rows={5}
+          name="description"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <div className="flexStart gap-2 mb-2">
+          <input
+            className="w-fit mb-0 accent-sky-800"
+            type="checkbox"
+            value="To-Do"
+            onChange={statusCheckHandler}
+            checked={statusCheck.includes("To-Do")}
+          />
+          <span>To-Do</span>
+        </div>
+        <div className="flexStart gap-2 mb-2">
+          <input
+            className="w-fit mb-0 accent-sky-800"
+            type="checkbox"
+            value="Blocked"
+            onChange={statusCheckHandler}
+            checked={statusCheck.includes("Blocked")}
+          />
+          <span>Blocked</span>
+        </div>
+        <div className="flexStart gap-2 mb-2">
+          <input
+            className="w-fit mb-0 accent-sky-800"
+            type="checkbox"
+            value="In Progress"
+            onChange={statusCheckHandler}
+            checked={statusCheck.includes("In Progress")}
+          />
+          <span>In Progress</span>
+        </div>
+        <div className="flexStart gap-2 mb-2">
+          <input
+            className="w-fit mb-0 accent-sky-800"
+            type="checkbox"
+            value="Awaiting QA"
+            onChange={statusCheckHandler}
+            checked={statusCheck.includes("Awaiting QA")}
+          />
+          <span>Awaiting QA</span>
+        </div>
+        <div className="flexStart gap-2 mb-2">
+          <input
+            className="w-fit mb-0 accent-sky-800"
+            type="checkbox"
+            value="In QA"
+            onChange={statusCheckHandler}
+            checked={statusCheck.includes("In QA")}
+          />
+          <span>In QA</span>
+        </div>
+        <div className="flexStart gap-2 mb-2">
+          <input
+            className="w-fit mb-0 accent-sky-800"
+            type="checkbox"
+            value="Won't Do"
+            onChange={statusCheckHandler}
+            checked={statusCheck.includes("Won't Do")}
+          />
+          <span>Won't Do</span>
+        </div>
+        <div className="flexStart gap-2 mb-2">
+          <input
+            className="w-fit mb-0 accent-sky-800"
+            type="checkbox"
+            value="Completed"
+            onChange={statusCheckHandler}
+            checked={statusCheck.includes("Completed")}
+          />
+          <span>Completed</span>
+        </div>
+        {error && <p className="text-red-400">{error}</p>}
+        <button
+          type="submit"
+          className="bg-pink-600 text-white w-full py-2 px-3 mt-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateSprintModal;
