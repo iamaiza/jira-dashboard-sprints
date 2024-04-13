36babaf891cb82dbdad9375fafcb362aeee34473
Aiza Ali <aiza9382@gmail.com>
2024-04-13 11:21:27 +0500

    profile updated

--- frontend/app/(root)/profile/[user_name]/page.tsx
+++ frontend/app/(root)/profile/[user_name]/page.tsx
@@ -2,7 +2,12 @@
 
 import UserInputs from "@/components/user-inputs";
 import useCurrentUser from "@/context/CurrentUserContext";
-import { EmailIcon } from "@/icons/icons";
+import {
+  EmailIcon,
+  HighPriorityIcon,
+  HigherPriorityIcon,
+  HighestPriorityIcon,
+} from "@/icons/icons";
 import { TaskProps } from "@/types/types";
 import { TASKS } from "@/utils/query-mutations";
 import { useQuery } from "@apollo/client";
@@ -12,78 +17,78 @@ const Profile = () => {
   const { user } = useCurrentUser();
   const { data } = useQuery(TASKS);
   return (
-    <div className="mt-32 mb-5 max-w-5xl mx-auto flex justify-start items-start gap-3">
-      <div className="max-w-[17rem] w-full">
-        <div>
-          {user?.imgUrl ? (
-            <Image
-              className="rounded-full border-4 border-slate-900"
-              src={user?.imgUrl}
-              alt="user-img"
-              width={128}
-              height={128}
-            />
-          ) : (
-            <div className="uppercase tracking-widest w-32 h-32 bg-sky-900 rounded-full flexCenter text-5xl font-bold border-4">
-              {user?.name.substr(0, 2)}
-            </div>
-          )}
-        </div>
-        <div className="text-[1.7rem] font-semibold text-gray-500 mt-4">
-          {user?.name}
-        </div>
-        <div className="mt-9 shadow-2xl w-full p-5">
+    <div className="max-md:overflow-x-auto mt-32 mb-5">
+      <div className="max-w-5xl max-md:w-[50rem] mx-auto flex justify-start items-start gap-3">
+        <div className="max-w-[17rem] w-full">
           <div>
-            <span className="uppercase text-xs tracking-widest font-bold text-gray-500">
-              about
-            </span>
-            <UserInputs />
+            {user?.imgUrl ? (
+              <Image
+                className="rounded-full border-4 border-slate-900"
+                src={user?.imgUrl}
+                alt="user-img"
+                width={128}
+                height={128}
+              />
+            ) : (
+              <div className="uppercase tracking-widest w-32 h-32 bg-sky-900 rounded-full flexCenter text-5xl font-bold border-4">
+                {user?.name.substr(0, 2)}
+              </div>
+            )}
           </div>
-          <div className="mt-6 cursor-not-allowed">
-            <span className="uppercase text-xs tracking-widest font-bold text-gray-500">
-              contact
-            </span>
-            <div className="text-sm text-gray-500 mt-2 flex gap-3">
-              <EmailIcon />
-              <span className="">{user?.email}</span>
+          <div className="text-[1.7rem] font-semibold text-gray-500 mt-4">
+            {user?.name}
+          </div>
+          <div className="mt-9 shadow-2xl w-full p-5">
+            <div>
+              <span className="uppercase text-xs tracking-widest font-bold text-gray-500">
+                about
+              </span>
+              <UserInputs />
+            </div>
+            <div className="mt-6 cursor-not-allowed">
+              <span className="uppercase text-xs tracking-widest font-bold text-gray-500">
+                contact
+              </span>
+              <div className="text-sm text-gray-500 mt-2 flex gap-3">
+                <EmailIcon />
+                <span className="">{user?.email}</span>
+              </div>
             </div>
           </div>
         </div>
+        {data?.tasks && (
+          <table className="mt-2 w-full">
+            <thead>
+              <tr>
+                <th className="py-1">Id</th>
+                <th className="py-1">Title</th>
+                <th className="py-1">Assignee</th>
+                <th className="py-1">Priority</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.tasks
+                .filter((task: TaskProps) => task.assigneeId?.id === user?.id)
+                .map((task: TaskProps) => (
+                  <tr className="border-b border-slate-800">
+                    <td className="text-center py-1 text-sm">{task?.id}</td>
+                    <td className="text-center py-1 text-sm text-sky-500">
+                      {task?.title}
+                    </td>
+                    <td className="text-center py-1 text-sm">
+                      {task?.assigneeId?.name || user?.name}
+                    </td>
+                    <td className="flexCenter py-1">
+                      {task?.priority === "high" && <HighPriorityIcon />}
+                      {task?.priority === "highest" && <HigherPriorityIcon />}
+                      {task?.priority === "major" && <HighestPriorityIcon />}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        )}
       </div>
-      {data?.tasks && (
-        <table className="mt-2 w-full">
-          <thead>
-            <tr>
-              <th className="py-1">Id</th>
-              <th className="py-1">Title</th>
-              <th className="py-1">Assignee</th>
-              <th className="py-1">Priority</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.tasks
-              .filter((task: TaskProps) => task.assigneeId?.id === user?.id)
-              .map((task: TaskProps) => (
-                <tr className="border-b border-slate-800">
-                  <td className="text-center py-1 text-sm">{task?.id}</td>
-                  <td className="text-center py-1 text-sm text-sky-500">{task?.title}</td>
-                  <td className="text-center py-1 text-sm">
-                    {task?.assigneeId?.name || user?.name}
-                  </td>
-                  <td className="text-center py-1">
-                    <Image
-                      className="mx-auto"
-                      src={`/assets/${task?.priority}.png`}
-                      alt="priority"
-                      width={20}
-                      height={20}
-                    />
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      )}
     </div>
   );
 };
--- frontend/app/(root)/your-work/page.tsx
+++ frontend/app/(root)/your-work/page.tsx
@@ -1,16 +1,23 @@
-"use client"
+"use client";
 
 import useCurrentUser from "@/context/CurrentUserContext";
-import { HighPriorityIcon, HigherPriorityIcon, HighestPriorityIcon } from "@/icons/icons";
+import {
+  HighPriorityIcon,
+  HigherPriorityIcon,
+  HighestPriorityIcon,
+} from "@/icons/icons";
 import { TaskProps } from "@/types/types";
 import { TASKS } from "@/utils/query-mutations";
 import { useQuery } from "@apollo/client";
 import Image from "next/image";
+import Link from "next/link";
 
 const YourWork = () => {
   const { user } = useCurrentUser();
   const { data } = useQuery(TASKS);
-  const filterTaskByUser = data?.tasks?.filter((task: TaskProps) => task?.assigneeId?.id === user?.id)
+  const filterTaskByUser = data?.tasks?.filter(
+    (task: TaskProps) => task?.assigneeId?.id === user?.id
+  );
   return (
     <div className="max-sm:overflow-x-auto min-h-[87vh]">
       {filterTaskByUser?.length > 0 ? (
@@ -28,17 +35,27 @@ const YourWork = () => {
               .filter((task: TaskProps) => task.assigneeId?.id === user?.id)
               .map((task: TaskProps) => (
                 <tr className="border-b border-slate-800">
-                  <td className="text-center py-1 text-sm">{task?.id}</td>
+                  <td className="text-center py-1 text-sm">
+                    <Link className="block w-full" href={`/tasks/${task?.id}`}>
+                      {task?.id}
+                    </Link>
+                  </td>
                   <td className="text-center py-1 text-sm text-sky-500">
-                    {task?.title}
+                    <Link className="block w-full" href={`/tasks/${task?.id}`}>
+                      {task?.title.substr(0, 25)}...
+                    </Link>
                   </td>
                   <td className="text-center py-1 text-sm">
-                    {task?.assigneeId?.name || user?.name}
+                    <Link className="block w-full" href={`/tasks/${task?.id}`}>
+                      {task?.assigneeId?.name || user?.name}
+                    </Link>
                   </td>
-                  <td className="flex justify-center py-1">
-                    {task?.priority === "high" && <HighPriorityIcon />}
-                    {task?.priority === "highest" && <HigherPriorityIcon />}
-                    {task?.priority === "major" && <HighestPriorityIcon />}
+                  <td className="py-1">
+                    <Link className="w-full flex justify-center " href={`/tasks/${task?.id}`}>
+                      {task?.priority === "high" && <HighPriorityIcon />}
+                      {task?.priority === "highest" && <HigherPriorityIcon />}
+                      {task?.priority === "major" && <HighestPriorityIcon />}
+                    </Link>
                   </td>
                 </tr>
               ))}
