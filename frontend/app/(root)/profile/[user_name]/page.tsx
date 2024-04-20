a2ef949797376fd4d5bd865800da61a9122dd62b
Aiza Ali <aiza9382@gmail.com>
2024-04-20 10:14:31 +0500

    user image edit added

--- frontend/app/(root)/profile/[user_name]/page.tsx
+++ frontend/app/(root)/profile/[user_name]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ChangeEvent, useState, useEffect } from "react";
 import ProfilePhoto from "@/components/ProfilePhoto";
 import UserInputs from "@/components/user-inputs";
 import useCurrentUser from "@/context/CurrentUserContext";
@@ -10,19 +11,91 @@ import {
   HighestPriorityIcon,
 } from "@/icons/icons";
 import { TaskProps } from "@/types/types";
-import { TASKS } from "@/utils/query-mutations";
-import { useQuery } from "@apollo/client";
+import { TASKS, UPDATE_IMG_URL } from "@/utils/query-mutations";
+import { useQuery, useMutation } from "@apollo/client";
 import Image from "next/image";
 
 const Profile = () => {
   const { user } = useCurrentUser();
   const { data } = useQuery(TASKS);
+  const [updateProfilePhotoMutation] = useMutation(UPDATE_IMG_URL);
+  const [userImg, setUserImg] = useState("");
+
+  useEffect(() => {
+    if (user?.imgUrl) setUserImg(user.imgUrl);
+  }, [user?.imgUrl]);
+
+  const fileChangeHandler = async (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        const result = event.target?.result;
+        if (typeof result === "string" || result instanceof ArrayBuffer) {
+          setUserImg(result);
+          try {
+            const { data } = await updateProfilePhotoMutation({
+              variables: {
+                data: {
+                  id: user?.id,
+                  imgUrl: result,
+                },
+              },
+            });
+            // setUserImg(data?.updateProfilePhoto?.updatedImgUrl);
+          } catch (error) {}
+        }
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+  const updateImgHandler = async (id: string) => {
+    document.getElementById(id)?.click();
+  };
+
   return (
     <div className="max-md:overflow-x-auto mt-32 mb-5">
       <div className="max-w-5xl max-md:w-[50rem] mx-auto flex justify-start items-start gap-3">
         <div className="max-w-[17rem] w-full">
           <div>
-            <ProfilePhoto imgUrl={user?.imgUrl} name={user?.name as string} />
+            {user && userImg ? (
+              <>
+                <figure
+                  className="w-28 h-28 relative"
+                  onClick={() => updateImgHandler("imgFile1")}
+                >
+                  <Image
+                    className="rounded-full object-cover"
+                    src={userImg}
+                    alt={user?.name}
+                    fill
+                  />
+                </figure>
+                <input
+                  type="file"
+                  id="imgFile1"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={fileChangeHandler}
+                />
+              </>
+            ) : (
+              <>
+                <div
+                  className="uppercase tracking-widest w-28 h-28 bg-sky-950 rounded-full flexCenter text-2xl font-bold"
+                  onClick={() => updateImgHandler("imgFile2")}
+                >
+                  {user?.name?.substr(0, 2)}
+                </div>
+                <input
+                  type="file"
+                  id="imgFile2"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={fileChangeHandler}
+                />
+              </>
+            )}
           </div>
           <div className="text-[1.7rem] font-semibold text-gray-500 mt-4">
             {user?.name}
@@ -45,7 +118,7 @@ const Profile = () => {
             </div>
           </div>
         </div>
-        {data?.tasks && (
+        {data?.tasks.filter((task: TaskProps) => task?.assigneeId?.id === user?.id).length > 0 && (
           <table className="mt-2 w-full">
             <thead>
               <tr>
